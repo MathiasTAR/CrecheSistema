70050cbe122e903530eeecb0d05ffc2752acdd2e
Gheorghe Bastos <gheorghe.bastos.ofc@gmail.com>
2025-10-23 20:22:45 -0300

    Implementação do atributo descricao e atributo usuarioRegistro

--- src/main/java/creche/model/HistoricoMatricula.java
+++ src/main/java/creche/model/HistoricoMatricula.java
@@ -7,16 +7,28 @@ public class HistoricoMatricula {
     private Long idMatricula;
     private LocalDateTime dataEvento;
     private String tipoEvento;
+    private String descricao;
+    private String usuarioRegistro;
     private String observacao;
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public Long getIdMatricula() { return idMatricula; }
     public void setIdMatricula(Long idMatricula) { this.idMatricula = idMatricula; }
+
     public LocalDateTime getDataEvento() { return dataEvento; }
     public void setDataEvento(LocalDateTime dataEvento) { this.dataEvento = dataEvento; }
+
     public String getTipoEvento() { return tipoEvento; }
     public void setTipoEvento(String tipoEvento) { this.tipoEvento = tipoEvento; }
+
+    public  String getDescricao() {return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public String getUsuarioRegistro() {return usuarioRegistro; }
+    public void setUsuarioRegistro(String usuarioRegistro) {this.usuarioRegistro = usuarioRegistro; }
+
     public String getObservacao() { return observacao; }
     public void setObservacao(String observacao) { this.observacao = observacao; }
 }
